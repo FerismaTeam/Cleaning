2f60cced489a51bd15e421ff7511c1197f0b5819
kunaal438 <kunaal438@gmail.com>
2026-01-18 12:08:52 +0530

    switched to next standalone output

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
     experimental: {
-    optimizePackageImports: ["@chakra-ui/react"],
-  },
+      optimizePackageImports: ["@chakra-ui/react"],
+    },
+    output: "standalone"
 };
 
 export default nextConfig;
