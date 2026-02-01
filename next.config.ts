f2c3156c5b4420ff98ce9fe844dc6ca3524827a9
kunaal438 <kunaal438@gmail.com>
2026-02-01 10:37:57 +0530

    first deploy

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,18 @@
 import type { NextConfig } from "next";
 
+const repo = '';
+
 const nextConfig: NextConfig = {
   /* config options here */
     experimental: {
       optimizePackageImports: ["@chakra-ui/react"],
     },
-    output: "export"
+    basePath: `/${repo}`,
+    assetPrefix: `/${repo}`,
+    output: "export",
+    images: {
+      unoptimized: true // required for github pages
+    }
 };
 
 export default nextConfig;
